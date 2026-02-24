e32a8a8e465bfc9125e66f489d2a79514afafed2
Slaveyko Zafirov <slaveyko.zafirov@yahoo.com>
2026-02-24 10:19:39 +0000

    fix main entry

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,16 @@
-// const db = require("../db/connection");
-// const app = require("../app");
-// const data = require("../db/data/test-data/index");
-// const { TestWatcher } = require("jest");
+const db = require("../db/connection");
+const app = require("../app");
+const data = require("../db/data/test-data/index");
+const { TestWatcher } = require("jest");
 
-// describe("GET: /api/topics", () => {
-//   test("200: Each topic object has category prop", () => {
-//     return request(app)
-//       .get("api/topics")
-//       .then(({ body: { topics } }) => {
-//         topics.forEach((topic) => {
-//           expect(topic.category.toBeString());
-//         });
-//       });
-//   });
-// });
+describe("GET: /api/topics", () => {
+  test("200: Each topic object has category prop", () => {
+    return request(app)
+      .get("api/topics")
+      .then(({ body: { topics } }) => {
+        topics.forEach((topic) => {
+          expect(topic.category.toBeString());
+        });
+      });
+  });
+});
