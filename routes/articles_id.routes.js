39dc1c5fea054db2f6570f38c38f2701f80cd343
Slaveyko Zafirov <slaveyko.zafirov@yahoo.com>
2026-02-24 10:33:22 +0000

    fix main entry

--- routes/articles_id.routes.js
+++ routes/articles_id.routes.js
@@ -1,3 +1,10 @@
+// const express = require("express");
+// const { getArticlesById } = require("../controllers/articles_id.controller");
+// const articlesByIdRouter = express.Router();
+// articlesByIdRouter.get("/:article_id", getArticlesById);
+
+// module.exports = articlesByIdRouter;
+
 const express = require("express");
 const { getArticlesById } = require("../controllers/articles_id.controller");
 const articlesByIdRouter = express.Router();
--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -1,12 +1,27 @@
+// const express = require("express");
+// const {
+//   getCommentsByArticleId,
+//   postCommentByArticleId,
+//   deleteComment,
+// } = require("../controllers/comments.controller");
+// const commentsRouter = express.Router();
+// commentsRouter.get("/:article_id/comments", getCommentsByArticleId);
+// commentsRouter.post("/:article_id/comments", postCommentByArticleId);
+// commentsRouter.delete("/:comment_id, deleteComment");
+
+// module.exports = commentsRouter;
+
 const express = require("express");
 const {
   getCommentsByArticleId,
   postCommentByArticleId,
   deleteComment,
 } = require("../controllers/comments.controller");
+
 const commentsRouter = express.Router();
+
 commentsRouter.get("/:article_id/comments", getCommentsByArticleId);
 commentsRouter.post("/:article_id/comments", postCommentByArticleId);
-commentsRouter.delete("/:comment_id, deleteComment");
+commentsRouter.delete("/:comment_id", deleteComment);
 
 module.exports = commentsRouter;
