bdb8210ff415660a07aabe693f1c5fa49922a5fe
Slaveyko Zafirov <slaveyko.zafirov@yahoo.com>
2026-02-24 09:56:45 +0000

    fix main entry

--- listen.js
+++ listen.js
@@ -1,5 +1,12 @@
+// const app = require("./app");
+// const PORT = 9090;
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
+
 const app = require("./app");
-const PORT = 9090;
+const PORT = process.env.PORT || 10000;
+
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
